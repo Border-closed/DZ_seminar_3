64f47b31c5e0ec96aed46fbd867fe2135765939f
Dmitrii <directsdi@gmail.com>
2022-10-13 23:03:41 +0500

    Вывод таблицы кубов

---
+++ task_3_table3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+try
+{
+double N = double.Parse(Console.ReadLine());
+double start = 1;
+while (start < N+1)
+{
+    double x = Math.Pow(start,3);
+    Console.Write(x + ", ");
+    start = start+1;
+}
+}
+catch
+{
+    Console.WriteLine("Вы ввели не число");
+}
